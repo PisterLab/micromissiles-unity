050bddd3ac17055fab55a4d66cc1462e4724b530
Daniel Lovell <dlovell98@eecs.berkeley.edu>
2024-10-09 15:08:43 -0700

    Remove UnityEditor.Animations

--- Assets/Scripts/Managers/ParticleManager.cs
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using UnityEditor.Animations;
 
 public class ParticleManager : MonoBehaviour {
   public static ParticleManager Instance { get; private set; }
