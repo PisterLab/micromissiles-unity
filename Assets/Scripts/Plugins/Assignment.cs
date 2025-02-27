22dad0703ea72320293a2f64bcaee08589849c7f
Titan Yuan <tryuan99@gmail.com>
2025-02-27 10:40:54 -0800

    Remove lib prefix when loading shared library

--- Assets/Scripts/Plugins/Assignment.cs
+++ Assets/Scripts/Plugins/Assignment.cs
@@ -2,22 +2,14 @@ using System;
 using System.Runtime.InteropServices;
 
 public class Assignment {
-// Assign the agents to the tasks using a cover assignment.
-#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+  // Assign the agents to the tasks using a cover assignment.
   [DllImport("assignment")]
-#else   // !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
-  [DllImport("libassignment")]
-#endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_CoverAssignment_Assign(int numAgents, int numTasks,
                                                              float[] costs, IntPtr assignedAgents,
                                                              IntPtr assignedTasks);
 
-// Assign the agents to the tasks using an even assignment.
-#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+  // Assign the agents to the tasks using an even assignment.
   [DllImport("assignment")]
-#else   // !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
-  [DllImport("libassignment")]
-#endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_EvenAssignment_Assign(int numAgents, int numTasks,
                                                             float[] costs, IntPtr assignedAgents,
                                                             IntPtr assignedTasks);
