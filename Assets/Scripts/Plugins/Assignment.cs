a3b74a7ac99550723a69cdb4cc33dd875e5b056d
Daniel Lovell <lovell.daniel92@gmail.com>
2025-02-26 23:50:33 -0800

    Fix preprocessor directives to detect Windows for plugins
    Need to use UNITY_EDITOR_WIN and UNITY_STANDALONE_WIN instead

--- Assets/Scripts/Plugins/Assignment.cs
+++ Assets/Scripts/Plugins/Assignment.cs
@@ -3,21 +3,21 @@ using System.Runtime.InteropServices;
 
 public class Assignment {
 // Assign the agents to the tasks using a cover assignment.
-#if UNITY_WIN
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   [DllImport("assignment")]
-#else   // !UNITY_WIN
+#else   // !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
   [DllImport("libassignment")]
-#endif  // UNITY_WIN
+#endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_CoverAssignment_Assign(int numAgents, int numTasks,
                                                              float[] costs, IntPtr assignedAgents,
                                                              IntPtr assignedTasks);
 
 // Assign the agents to the tasks using an even assignment.
-#if UNITY_WIN
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   [DllImport("assignment")]
-#else   // !UNITY_WIN
+#else   // !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
   [DllImport("libassignment")]
-#endif  // UNITY_WIN
+#endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_EvenAssignment_Assign(int numAgents, int numTasks,
                                                             float[] costs, IntPtr assignedAgents,
                                                             IntPtr assignedTasks);
