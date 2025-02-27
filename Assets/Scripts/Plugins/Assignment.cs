65f0c9886145695903b652b5d5b4d4c23332681e
Titan Yuan <tryuan99@gmail.com>
2025-02-27 05:46:28 -0800

    Fix inverse of $if

--- Assets/Scripts/Plugins/Assignment.cs
+++ Assets/Scripts/Plugins/Assignment.cs
@@ -5,7 +5,7 @@ public class Assignment {
 // Assign the agents to the tasks using a cover assignment.
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   [DllImport("assignment")]
-#else   // !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
+#else   // !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
   [DllImport("libassignment")]
 #endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_CoverAssignment_Assign(int numAgents, int numTasks,
@@ -15,7 +15,7 @@ public class Assignment {
 // Assign the agents to the tasks using an even assignment.
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   [DllImport("assignment")]
-#else   // !UNITY_EDITOR_WIN || !UNITY_STANDALONE_WIN
+#else   // !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
   [DllImport("libassignment")]
 #endif  // UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
   public static extern int Assignment_EvenAssignment_Assign(int numAgents, int numTasks,
