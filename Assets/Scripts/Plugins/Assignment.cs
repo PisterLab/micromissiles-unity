32f7a351f24f5a4b0a267d3a5bacb9b6bb0f798a
Titan Yuan <tryuan99@gmail.com>
2025-02-27 10:56:34 -0800

    Update shared libraries with weighted even assignment

--- Assets/Scripts/Plugins/Assignment.cs
+++ Assets/Scripts/Plugins/Assignment.cs
@@ -13,4 +13,12 @@ public class Assignment {
   public static extern int Assignment_EvenAssignment_Assign(int numAgents, int numTasks,
                                                             float[] costs, IntPtr assignedAgents,
                                                             IntPtr assignedTasks);
+
+  // Assign the agents to the tasks using a weighted even assignment.
+  [DllImport("assignment")]
+  public static extern int Assignment_WeightedEvenAssignment_Assign(int numAgents, int numTasks,
+                                                                    float[] costs, float[] weights,
+                                                                    int weightScalingFactor,
+                                                                    IntPtr assignedAgents,
+                                                                    IntPtr assignedTasks);
 }
