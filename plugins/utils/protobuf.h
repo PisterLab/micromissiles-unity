8ee47bc06c95c86a47e227739a544601c8b842f6
Titan Yuan <tryuan99@gmail.com>
2025-07-30 23:39:09 +0200

    Add Protobuf utilities

---
+++ plugins/utils/protobuf.h
@@ -0,0 +1,34 @@
+// This file defines some useful utilities for Protobuf.
+
+#pragma once
+
+#include <fcntl.h>
+
+#include <stdexcept>
+#include <string>
+
+#include "absl/strings/str_format.h"
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "google/protobuf/text_format.h"
+
+namespace utils {
+
+// Load the Protobuf text file.
+template <typename T>
+T LoadProtobufTextFile(const std::string& file) {
+  const int fd = open(file.c_str(), O_RDONLY);
+  if (fd < 0) {
+    throw std::runtime_error(
+        absl::StrFormat("Failed to open the Protobuf text file: %s", file));
+  }
+  google::protobuf::io::FileInputStream file_stream(fd);
+  T message;
+  if (!google::protobuf::TextFormat::Parse(&file_stream, &message)) {
+    throw std::runtime_error(
+        absl::StrFormat("Failed to parse the Protobuf text file: %s.", file));
+  }
+  file_stream.Close();
+  return message;
+}
+
+}  // namespace utils
