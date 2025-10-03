c1975d7b547ad7f2654b090cd6efb1a6d3dcdce1
Titan Yuan <tryuan99@gmail.com>
2025-10-03 23:51:25 +0200

    [Config] Add Protobuf configurations (#55)
    * Define protos for simulation configurations
    
    * Add Bazel build file to protos
    
    * Add example protobuf configurations
    
    * Add Protobuf utilities
    
    * Add Protobuf plugin
    
    * Add Google.Protobuf C# plugin to Unity
    
    * Move generated Protobuf files under Assets/Scripts
    
    * Update metadata for Protobuf files
    
    * Address comments
    
    * Regenerate plugin libraries
    
    * Enable optimized builds by default
    
    * Add metadata file for Protobuf plugin
    
    * Move generated files into dedicated Generated/ directory
    
    * Add Windows def files for example and protobuf plugins
    
    * Export def files from package
    
    * Regenerate plugins
    
    * Regenerate Protobuf plugin

---
+++ plugins/protobuf/protobuf_test.cc
@@ -0,0 +1,48 @@
+#include "protobuf/protobuf.h"
+
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "configs/proto/static_config.pb.h"
+
+namespace protobuf {
+namespace {
+
+TEST(ProtobufTest, LoadProtobufTextFileTest) {
+  const std::string kStaticConfigFile =
+      "configs/interceptors/micromissile.pbtxt";
+  const auto static_config =
+      LoadProtobufTextFile<micromissiles::StaticConfig>(kStaticConfigFile);
+  EXPECT_TRUE(static_config.has_acceleration_config());
+  EXPECT_TRUE(static_config.has_boost_config());
+  EXPECT_TRUE(static_config.has_lift_drag_config());
+  EXPECT_TRUE(static_config.has_body_config());
+  EXPECT_FALSE(static_config.has_hit_config());
+}
+
+TEST(ProtobufTest, SerializeToBufferTest) {
+  const std::string kStaticConfigFile =
+      "configs/interceptors/micromissile.pbtxt";
+  const auto static_config =
+      LoadProtobufTextFile<micromissiles::StaticConfig>(kStaticConfigFile);
+  std::vector<uint8_t> buffer(1024);
+  EXPECT_NO_THROW(
+      SerializeToBuffer(static_config, buffer.data(), buffer.size()));
+}
+
+TEST(ProtobufTest, SerializeToBufferInsufficientSizeTest) {
+  const std::string kStaticConfigFile =
+      "configs/interceptors/micromissile.pbtxt";
+  const auto static_config =
+      LoadProtobufTextFile<micromissiles::StaticConfig>(kStaticConfigFile);
+  std::vector<uint8_t> buffer(1);
+  EXPECT_THROW(SerializeToBuffer(static_config, buffer.data(), buffer.size()),
+               std::runtime_error);
+}
+
+}  // namespace
+}  // namespace protobuf
