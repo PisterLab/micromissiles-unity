8a3c41998d5474a26cabaa1e32319459000bbf1d
Titan Yuan <tryuan99@gmail.com>
2025-10-07 15:22:41 -0700

    [Plugins] Remove unsafe blocks from plugin function calls (#83)
    * Remove unsafe blocks from plugin function calls
    
    * Address comments
    
    * Change csharp to cs

--- Assets/Scripts/Plugins/Protobuf.cs
+++ Assets/Scripts/Plugins/Protobuf.cs
@@ -6,47 +6,47 @@ public static class Protobuf {
   // serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_AttackBehaviorConfig_GetSerializedLength(
-      string file, IntPtr serializedLength);
+      string file, out int serializedLength);
 
   // Load an attack behavior configuration from a Protobuf text file to binary format and return
   // the length of the serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_AttackBehaviorConfig_LoadToBinary(
-      string file, IntPtr buffer, int size, IntPtr serializedLength);
+      string file, byte[] buffer, int size, out int serializedLength);
 
   // Load a simulation configuration from a Protobuf text file and return the length of the
   // serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_SimulationConfig_GetSerializedLength(
-      string file, IntPtr serializedLength);
+      string file, out int serializedLength);
 
   // Load a simulation configuration from a Protobuf text file to binary format and return the
   // length of the serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_SimulationConfig_LoadToBinary(
-      string file, IntPtr buffer, int size, IntPtr serializedLength);
+      string file, byte[] buffer, int size, out int serializedLength);
 
   // Load an simulator configuration from a Protobuf text file and return the length of the
   // serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_SimulatorConfig_GetSerializedLength(
-      string file, IntPtr serializedLength);
+      string file, out int serializedLength);
 
   // Load a simulator configuration from a Protobuf text file to binary format and return the length
   // of the serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_SimulatorConfig_LoadToBinary(
-      string file, IntPtr buffer, int size, IntPtr serializedLength);
+      string file, byte[] buffer, int size, out int serializedLength);
 
   // Load a static configuration from a Protobuf text file and return the length of the serialized
   // message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_StaticConfig_GetSerializedLength(
-      string file, IntPtr serializedLength);
+      string file, out int serializedLength);
 
   // Load a static configuration from a Protobuf text file to binary format and return the length of
   // the serialized message as an output argument.
   [DllImport("protobuf")]
   public static extern Plugin.StatusCode Protobuf_StaticConfig_LoadToBinary(
-      string file, IntPtr buffer, int size, IntPtr serializedLength);
+      string file, byte[] buffer, int size, out int serializedLength);
 }
