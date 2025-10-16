820eb7d6b73ee6467ae3fb43eeb180ad7d30d1bb
Titan Yuan <tryuan99@gmail.com>
2025-10-16 10:58:17 -0700

    Add hierarchical as property of predictor clases

--- Assets/Scripts/Algorithms/Prediction/IPredictor.cs
+++ Assets/Scripts/Algorithms/Prediction/IPredictor.cs
@@ -2,6 +2,8 @@
 //
 // The predictor predicts the trajectories of hierarchical objects.
 public interface IPredictor {
+  IHierarchical Hierarchical { get; set; }
+
   // Predict the future state of the hierarchical object.
-  PredictorState Predict(IHierarchical hierarchical, float time);
+  PredictorState Predict(float time);
 }
--- Assets/Scripts/Algorithms/Prediction/LinearExtrapolator.cs
+++ Assets/Scripts/Algorithms/Prediction/LinearExtrapolator.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 // The linear extrapolator class predicts the trajectory of an agent by linearly extrapolating its
 // current position and velocity.
 public class LinearExtrapolator : PredictorBase {
+  public LinearExtrapolator(IHierarchical hierarchical) : base(hierarchical) {}
+
   // Predict the future state of the hierarchical object by linearly extrapolating its current
   // position and velocity.
-  public override PredictorState Predict(IHierarchical hierarchical, float time) {
-    var position = hierarchical.Position + hierarchical.Velocity * time;
+  public override PredictorState Predict(float time) {
+    var position = Hierarchical.Position + Hierarchical.Velocity * time;
     return new PredictorState {
       Position = position,
-      Velocity = hierarchical.Velocity,
-      Acceleration = hierarchical.Acceleration,
+      Velocity = Hierarchical.Velocity,
+      Acceleration = Hierarchical.Acceleration,
     };
   }
 }
