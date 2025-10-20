50fe728facd4bdfed380a91df42d15d386870ade
Daniel Lovell <dlovell98@eecs.berkeley.edu>
2025-10-19 23:36:29 -0700

    Fix GenerateSolutionFiles

--- Assets/Editor/GenerateSolutionFiles.cs
+++ Assets/Editor/GenerateSolutionFiles.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+using Unity.CodeEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +9,18 @@ public static class GenerateSolutionFiles
     {
         Debug.Log("Forcing solution file generation...");
 
-        // This syncs all code projects
-        UnityEditor.SyncVS.SyncSolution();
+        var editor = CodeEditor.CurrentEditor;
+        if (editor != null)
+        {
+            editor.SyncAll();
+            Debug.Log($"Synced external code editor: {editor.GetType().Name}");
+        }
+        else
+        {
+            Debug.LogWarning("No external code editor is registered; skipping solution sync.");
+        }
 
         Debug.Log("Solution generation complete!");
     }
 }
+#endif
