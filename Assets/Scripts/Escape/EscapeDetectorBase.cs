be8cbff762efae427ef7a52b9ec378c1d41d4e41
Titan Yuan <tryuan99@gmail.com>
2025-11-04 22:08:00 -0800

    Add escape detectors

---
+++ Assets/Scripts/Escape/EscapeDetectorBase.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+// Base implementation for an escape detector.
+public abstract class EscapeDetectorBase : IEscapeDetector {
+  // Agent that is pursuing the target.
+  public IAgent Agent { get; init; }
+
+  public EscapeDetectorBase(IAgent agent) {
+    Agent = agent;
+  }
+
+  // Determine whether the target is escaping the agent.
+  public abstract bool IsEscaping(IHierarchical target);
+}
