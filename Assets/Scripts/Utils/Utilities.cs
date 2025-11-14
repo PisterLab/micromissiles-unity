5697460abaa3634471b7e1259cd7a31cbb13205d
Titan Yuan <tryuan99@gmail.com>
2025-11-13 23:13:43 -0800

    Sample from normal distribution

--- Assets/Scripts/Utils/Utilities.cs
+++ Assets/Scripts/Utils/Utilities.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public static class Utilities {
   public static Vector3 GenerateRandomNoise(Vector3 standardDeviation) {
-    return new Vector3(Random.Range(-standardDeviation.x, standardDeviation.x),
-                       Random.Range(-standardDeviation.y, standardDeviation.y),
-                       Random.Range(-standardDeviation.z, standardDeviation.z));
+    return new Vector3(SampleStandardNormal() * standardDeviation.x,
+                       SampleStandardNormal() * standardDeviation.y,
+                       SampleStandardNormal() * standardDeviation.z);
   }
 
   public static Vector3 GenerateRandomNoise(Simulation.CartesianCoordinates standardDeviation) {
     return GenerateRandomNoise(Coordinates3.FromProto(standardDeviation));
+  }
+
+  public static float SampleStandardNormal() {
+    // Use the Box-Muller transform to sample from a standard normal distribution with mean = 0 and
+    // standard deviation = 1.
+    float u1 = 1.0f - Random.value;
+    float u2 = 1.0f - Random.value;
+    return Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Sin(2.0f * Mathf.PI * u2);
   }
 
   public static float ConvertMpsToKnots(float mps) {
