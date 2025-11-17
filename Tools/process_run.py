09fbc7e27b9f6b143b47bd3100f86eafd019e34a
Titan Yuan <tryuan99@gmail.com>
2025-11-17 09:56:07 -0800

    Process the aggregated logs of a simulation run

---
+++ Tools/process_run.py
@@ -0,0 +1,122 @@
+"""Processes the logs of a simulation run."""
+
+import matplotlib.pyplot as plt
+import mpl_config
+import numpy as np
+import pandas as pd
+import utils
+from absl import app, flags, logging
+from aggregator import Aggregator
+from distribution import Distribution
+from multi_metric import InterceptPosition2D, MultiMetric
+from scalar_metric import (HitRate, InterceptorEfficiency,
+                           MinInterceptorDistance, NumHits, NumInterceptors,
+                           NumMisses)
+
+FLAGS = flags.FLAGS
+
+# Pixel size in meters.
+PIXEL_SIZE = 20
+
+# Scalar metrics.
+SCALAR_METRICS = [
+    NumInterceptors(),
+    NumHits(),
+    NumMisses(),
+    HitRate(),
+    InterceptorEfficiency(),
+    MinInterceptorDistance(),
+]
+
+
+def print_aggregated_stats(event_dfs: list[pd.DataFrame]) -> None:
+    """Prints the aggregated statistics of the simulation run.
+
+    Args:
+        event_dfs: List of dataframes containing the events.
+    """
+    num_runs = len(event_dfs)
+    logging.info(
+        "Aggregating the stats for %d runs found in the log directory.",
+        num_runs)
+
+    for metric in SCALAR_METRICS:
+        distribution = Distribution(event_dfs, metric)
+        mean = distribution.mean()
+        std = distribution.std()
+        logging.info("  %s: mean: %f, std: %f.", metric.name, mean, std)
+
+
+def plot_heatmap(event_dfs: list[pd.DataFrame], metric: MultiMetric) -> None:
+    """Plots the heatmap of the multi-metric aggregated over all simulation runs.
+
+    Args:
+        event_dfs: List of dataframes containing the events.
+        metric: Multi-metric outputting 2D coordinates.
+    """
+    aggregator = Aggregator(event_dfs, metric)
+    values = np.array(aggregator.values)
+
+    # Calculate the extent to plot.
+    xvalues = values[:, 0]
+    xmin = min(xvalues)
+    xmax = max(xvalues)
+    xrange = xmax - xmin
+    yvalues = values[:, 1]
+    ymin = min(yvalues)
+    ymax = max(yvalues)
+    yrange = ymax - ymin
+    xpad = xrange * 0.05 if xrange > 0 else 1
+    ypad = yrange * 0.05 if yrange > 0 else 1
+    xedges = np.linspace(xmin - xpad, xmax + xpad, int(xrange / PIXEL_SIZE) + 1)
+    yedges = np.linspace(ymin - ypad, ymax + ypad, int(yrange / PIXEL_SIZE) + 1)
+
+    # Plot a 2D histogram of the metric values.
+    fig, ax = plt.subplots(figsize=(16, 8))
+    H, _, _ = np.histogram2d(values[:, 0], values[:, 1], bins=[xedges, yedges])
+    H = H.T
+    im = ax.imshow(
+        H,
+        cmap="hot",
+        aspect="equal",
+        interpolation="bilinear",
+        origin="lower",
+        extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+    )
+    plt.colorbar(im, ax=ax)
+    ax.set_xlabel("$x$ [m]")
+    ax.set_ylabel("$z$ [m]")
+    ax.set_title(metric.name)
+    ax.grid(alpha=0.25)
+    fig.tight_layout()
+    plt.show()
+
+    # Plot a scatter plot of the metric values.
+    fig, ax = plt.subplots(figsize=(16, 8))
+    ax.scatter(xvalues, yvalues, s=PIXEL_SIZE, c="red", alpha=0.8)
+    ax.set_aspect("equal")
+    ax.set_xlabel("$x$ [m]")
+    ax.set_ylabel("$z$ [m]")
+    ax.set_xlim(xedges[0], xedges[-1])
+    ax.set_ylim(yedges[0], yedges[-1])
+    ax.set_title(metric.name)
+    ax.grid(alpha=0.5)
+    fig.tight_layout()
+    plt.show()
+
+
+def main(argv):
+    assert len(argv) == 1, argv
+
+    event_log_paths = utils.find_all_event_logs(FLAGS.run_log_dir)
+    event_dfs = [utils.read_event_log(path) for path in event_log_paths]
+    print_aggregated_stats(event_dfs)
+    plot_heatmap(event_dfs, InterceptPosition2D())
+
+
+if __name__ == "__main__":
+    flags.DEFINE_string(
+        "run_log_dir", None,
+        "Run log directory containing subdirectories for the logs of each run.")
+
+    app.run(main)
