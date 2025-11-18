9afadb42946d79bbb87f70a13c8d9ca8bb817dda
Titan Yuan <tryuan99@gmail.com>
2025-11-17 18:23:45 -0800

    Change sans-serif fonts

--- Tools/mpl_config.py
+++ Tools/mpl_config.py
@@ -1,5 +1,9 @@
 import matplotlib
 
 matplotlib.rcParams["font.family"] = "sans-serif"
-matplotlib.rcParams["font.sans-serif"] = ["Helvetica Neue", "Segoe UI", "Arial"]
+matplotlib.rcParams["font.sans-serif"] = [
+    "Helvetica Neue",
+    "Nimbus Sans",
+    "Arial",
+]
 matplotlib.rcParams["lines.linewidth"] = 2
