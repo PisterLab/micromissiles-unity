9a9121f6f6e2e7999f23ab346743cfb99d4083c3
Titan Yuan <tryuan99@gmail.com>
2025-11-20 21:46:47 -0800

    Add Inter to font family

--- Tools/mpl_config.py
+++ Tools/mpl_config.py
@@ -4,6 +4,7 @@ matplotlib.rcParams["font.family"] = "sans-serif"
 matplotlib.rcParams["font.sans-serif"] = [
     "Helvetica Neue",
     "Nimbus Sans",
+    "Inter",
     "Arial",
 ]
 matplotlib.rcParams["lines.linewidth"] = 2
