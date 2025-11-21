8245f561047c657b27f14b2bb66e1c9568443ebd
Titan Yuan <tryuan99@gmail.com>
2025-11-21 10:10:24 -0800

    Adjust grid parameters

--- Tools/mpl_config.py
+++ Tools/mpl_config.py
@@ -1,6 +1,11 @@
 import matplotlib
 
+matplotlib.rcParams["axes.axisbelow"] = True
 matplotlib.rcParams["axes.grid"] = True
+matplotlib.rcParams["grid.alpha"] = 0.5
+matplotlib.rcParams["grid.color"] = "black"
+matplotlib.rcParams["grid.linestyle"] = "--"
+matplotlib.rcParams["figure.autolayout"] = True
 matplotlib.rcParams["font.family"] = "sans-serif"
 matplotlib.rcParams["font.sans-serif"] = [
     "Helvetica Neue",
