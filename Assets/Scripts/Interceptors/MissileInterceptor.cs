419d6200247cf9c85d11df3f0e996fd1dcd14b2c
Titan Yuan <tryuan99@gmail.com>
2025-12-09 18:36:35 -0800

    Fix build error

--- Assets/Scripts/Interceptors/MissileInterceptor.cs
+++ Assets/Scripts/Interceptors/MissileInterceptor.cs
@@ -3,10 +3,7 @@
 // The missile interceptor is a small, intelligent missile that tries to intercept a single threat.
 public class MissileInterceptor : InterceptorBase {
   // Maximum number of threats that this interceptor can target.
-  public override int Capacity {
-    get => 1;
-    set {}
-  }
+  public override int Capacity => 1;
 
   protected override void Awake() {
     base.Awake();
