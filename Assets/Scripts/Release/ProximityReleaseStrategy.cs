45bcfa35101694435f209e16eb4e8e5583d26145
Titan Yuan <tryuan99@gmail.com>
2025-12-10 08:23:02 -0800

    [Release] Adjust minimum distance in proximity release (#129)
    * Change proximity release strategy minimum distance to threat

--- Assets/Scripts/Release/ProximityReleaseStrategy.cs
+++ Assets/Scripts/Release/ProximityReleaseStrategy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class ProximityReleaseStrategy : MassReleaseStrategyBase {
   // Geometric parameters for determining when to launch.
   // The carrier interceptor will spawn submunitions when any target is greater than 30 degrees away
-  // from the carrier interceptor's current velocity or when any threat is within 500 meters of the
+  // from the carrier interceptor's current velocity or when any threat is within 1000 meters of the
   // interceptor.
   private const float _maxBearingDegrees = 30f;
-  private const float _minDistanceToThreat = 500f;
-  private const float _maxDistanceToThreat = 2000f;
+  private const float _minDistanceToThreat = 1000f;
+  private const float _maxDistanceToThreat = 2500f;
   // TODO(titan): The prediction time should be a function of the sub-interceptor characteristic,
   // such as the boost time.
   private const float _predictionTime = 0.6f;
