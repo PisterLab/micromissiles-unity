389327df09327aafb74eb4ac53d9dee041f2c887
Titan Yuan <tryuan99@gmail.com>
2025-12-15 12:31:23 -0800

    [Tools] Refactor common simulation-related constants and utilities (#160)
    * Refactor common simulation-related constants
    
    * Use pathlib instead of glob
    
    * Address comments
    
    * Change sans-serif fonts
    
    * Remove file extension from Linux build
    
    * Rename unity.py to unity_utils.py
    
    * Turn on grid by default
    
    * Adjust grid parameters
    
    * Fix renaming to unity_utils
    
    * Address comments
    
    * Fix merge
    
    * Address comments

--- Tools/unity_utils.py
+++ Tools/unity_utils.py
@@ -124,10 +124,24 @@ def find_unity_path(unity_path: str) -> Path:
 
     executables = _find_unity_executables()
     if not executables:
-        raise ValueError(
-            f"No Unity installations detected. Use --unity-path or set $UNITY_PATH."
-        )
+        raise ValueError("No Unity installations detected. "
+                         "Use --unity-path or set $UNITY_PATH.")
     if len(executables) > 1:
         logging.warning("Found multiple Unity installations.")
     logging.info("Found Unity installation at %s.", executables[0])
     return executables[0]
+
+
+def get_persistent_data_directory() -> str:
+    """Returns the path to Unity's persistent data directory."""
+    system = platform.system()
+    if system == "Windows":
+        return os.path.expandvars(
+            r"%USERPROFILE%\AppData\LocalLow\BAMLAB\micromissiles\Logs")
+    elif system == "Darwin":
+        return os.path.expanduser(
+            "~/Library/Application Support/BAMLAB/micromissiles/Logs")
+    elif system == "Linux":
+        return os.path.expanduser("~/.config/unity3d/BAMLAB/micromissiles/Logs")
+    else:
+        raise NotImplementedError(f"Unsupported platform: {system}.")
---
+++ Tools/utils.py
@@ -0,0 +1,124 @@
+"""Implements various utility functions."""
+
+from pathlib import Path
+
+import pandas as pd
+from absl import logging
+from constants import EVENT_LOG_FILE_PREFIX, TELEMETRY_FILE_PREFIX
+
+
+def find_all_files(directory: str, file_pattern: str) -> list[Path]:
+    """Returns all files in the directory and its subdirectories that match the
+    file pattern.
+
+    If no files match the given pattern, returns an empty list.
+
+    Args:
+        directory: Directory to look through.
+        file_pattern: File pattern to match.
+    """
+    files = list(Path(directory).rglob(file_pattern))
+    if not files:
+        logging.warning(f"No files found matching the pattern {file_pattern} "
+                        f"in the directory: {directory}.")
+    return files
+
+
+def find_all_telemetry_files(log_dir: str) -> list[Path]:
+    """Returns all telemetry files in the directory and its subdirectories.
+
+    Args:
+        log_dir: Log directory.
+    """
+    return find_all_files(log_dir, f"{TELEMETRY_FILE_PREFIX}_*.csv")
+
+
+def find_all_event_logs(log_dir: str) -> list[Path]:
+    """Returns all event logs in the directory and its subdirectories.
+
+    Args:
+        log_dir: Log directory.
+    """
+    return find_all_files(log_dir, f"{EVENT_LOG_FILE_PREFIX}_*.csv")
+
+
+def find_latest_file(directory: str, file_pattern: str) -> Path | None:
+    """Returns the latest file in the directory and its subdirectories that
+    matches the file pattern.
+
+    If no files match the given pattern, returns None.
+
+    Args:
+        directory: Directory to look through.
+        file_pattern: File pattern to match.
+    """
+    files = find_all_files(directory, file_pattern)
+    if not files:
+        return None
+    latest_file = max(files, key=lambda path: path.stat().st_mtime)
+    logging.info(f"Using latest file found: {latest_file}.")
+    return latest_file
+
+
+def find_latest_telemetry_file(log_dir: str) -> Path | None:
+    """Returns the latest telemetry file.
+
+    Args:
+        log_dir: Log directory.
+    """
+    return find_latest_file(log_dir, f"{TELEMETRY_FILE_PREFIX}_*.csv")
+
+
+def find_latest_event_log(log_dir: str) -> Path | None:
+    """Returns the latest event log.
+
+    Args:
+        log_dir: Log directory.
+    """
+    return find_latest_file(log_dir, f"{EVENT_LOG_FILE_PREFIX}_*.csv")
+
+
+def find_all_subdirectories(directory: str, subdir_pattern: str) -> list[Path]:
+    """Returns all subdirectories within the directory and its subdirectories
+    that match the subdirectory pattern.
+
+    If no subdirectories match the given pattern, returns an empty list.
+
+    Args:
+        directory: Directory to look through.
+        subdir_pattern: Subdirectory pattern to match.
+    """
+    paths = Path(directory).rglob(subdir_pattern)
+    subdirs = [path for path in paths if path.is_dir()]
+    if not subdirs:
+        logging.warning(
+            f"No subdirectories found matching the pattern {subdir_pattern} "
+            f"in the directory: {directory}.")
+    return subdirs
+
+
+def read_telemetry_file(path: str | Path) -> pd.DataFrame:
+    """Reads the telemetry file into a dataframe.
+
+    Args:
+        path: Path to the telemetry file.
+
+    Returns:
+        A dataframe containing the telemetry data.
+    """
+    return pd.read_csv(path)
+
+
+def read_event_log(path: str | Path) -> pd.DataFrame:
+    """Reads the event log file into a dataframe.
+
+    Args:
+        path: Path to the event log.
+
+    Returns:
+        A dataframe containing the events.
+    """
+    df = pd.read_csv(path)
+    # Sanitize the event column to ensure consistency.
+    df["Event"] = df["Event"].str.upper().str.strip()
+    return df
