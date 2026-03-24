7403d2651d83fe603a60b7e43b3ae0a282cdb68f
Joseph Chen <josephrchen120@berkeley.edu>
2026-03-23 18:30:30 -0700

    [Communication] Sets up MessagePayload

---
+++ Assets/Scripts/Agents/Messaging/MessagePayload.cs
@@ -0,0 +1,29 @@
+using System;
+
+// MessagePayload defines different types of new payload objects. Payload is carried my Message envolopes.
+
+public partial interface IMessagePayload {}
+
+public sealed class AssignSubInterceptorRequestPayload : IMessagePayload {
+  public IInterceptor SubInterceptor { get; }
+
+  public AssignSubInterceptorRequestPayload(IInterceptor subInterceptor) {
+    SubInterceptor = subInterceptor ?? throw new ArgumentNullException(nameof(subInterceptor));
+  }
+}
+
+public sealed class AssignTargetPayload : IMessagePayload {
+  public IHierarchical Target { get; }
+
+  public AssignTargetPayload(IHierarchical target) {
+    Target = target ?? throw new ArgumentNullException(nameof(target));
+  }
+}
+
+public sealed class ReassignTargetRequestPayload : IMessagePayload {
+  public IHierarchical Target { get; }
+
+  public ReassignTargetRequestPayload(IHierarchical target) {
+    Target = target ?? throw new ArgumentNullException(nameof(target));
+  }
+}
