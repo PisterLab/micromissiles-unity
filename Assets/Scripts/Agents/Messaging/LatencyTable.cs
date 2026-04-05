ae73009b073a0e02ae07769115f93408cac128c3
Joseph Chen <josephrchen120@berkeley.edu>
2026-04-05 13:40:20 -0700

    [Comms_Proto] Fixing LatencyTable enum casting

--- Assets/Scripts/Agents/Messaging/LatencyTable.cs
+++ Assets/Scripts/Agents/Messaging/LatencyTable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 // Latency Table keeps track of the delay between every agent.
@@ -10,15 +11,18 @@ public enum CommsNode {
     Interceptor = 2,
 }
 
-// Setting everything to 0 should behave like with no latency
+// Setting everything to 0 should behave like with no latency.
 public sealed class LatencyTable {
+    private static readonly CommsNode[] Nodes = (CommsNode[]) Enum.GetValues(typeof(CommsNode));
+    private static readonly Dictionary<CommsNode, int> NodeIndices = CreateNodeIndices();
     private readonly float[,] _latency;
 
-    // creates empty _latency array with defaultSeconds seconds each
+    // Creates empty _latency array with defaultSeconds seconds each.
     public LatencyTable(float defaultSeconds = 0f) {
-        int n = Enum.GetValues(typeof(CommsNode)).Length;
+        int n = Nodes.Length;
         _latency = new float[n, n];
-        for (int i = 0; i < n; ++i) {
+        if (defaultSeconds == 0f) { return; }
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 _latency[i, j] = defaultSeconds;
             }
@@ -26,10 +30,26 @@ public sealed class LatencyTable {
     }
 
     public void Set(CommsNode from, CommsNode to, float seconds) {
-        _latency[(int) from, (int) to] = Mathf.Max(0f, seconds);
+        _latency[GetIndex(from), GetIndex(to)] = Mathf.Max(0f, seconds);
     }
 
     public float Get(CommsNode from, CommsNode to) {
-        return _latency[(int) from, (int) to];
+        return _latency[GetIndex(from), GetIndex(to)];
+    }
+
+    // Used for quick GetIndex lookup and make sure enum always starts with 0 to N-1.
+    private static Dictionary<CommsNode, int> CreateNodeIndices() {
+        var indices = new Dictionary<CommsNode, int>(Nodes.Length);
+        for (int i = 0; i < Nodes.Length; ++i) {
+            indices[Nodes[i]] = i;
+        }
+        return indices;
+    }
+
+    private static int GetIndex(CommsNode node) {
+        if (!NodeIndices.TryGetValue(node, out int index)) {
+            throw new ArgumentOutOfRangeException(nameof(node), node, "Unknown Interceptor Type.");
+        }
+        return index;
     }
 }
