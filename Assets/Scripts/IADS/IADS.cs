85bbe8f8530e0f487657af2f168dc3b6f52f307a
Joseph Chen <josephrchen120@berkeley.edu>
2026-07-23 22:47:33 -0700

    Fixed Coderabbit Commetns

--- Assets/Scripts/IADS/IADS.cs
+++ Assets/Scripts/IADS/IADS.cs
@@ -47,7 +47,10 @@ public class IADS : MonoBehaviour, ICommsEndpoint {
 
     // Create a communication node for the IADS.
     CommsNode = new CommsNode(Configs.AgentType.Iads);
-    CommsManager.Instance.AddNode(CommsNode);
+    CommsManager commsManager = CommsManager.Instance;
+    if (commsManager != null) {
+      commsManager.AddNode(CommsNode);
+    }
   }
 
   private void OnDestroy() {
